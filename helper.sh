919864b9786c85f879f7a4583f51bbb62ca560b6
Boyd Zweers <boydzweers46@gmail.com>
2021-06-11 11:59:38 +0200

    Create helper.sh

---
+++ helper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
+ENDCOLOR=`tput sgr0`
+
+COMMIT_TYPE=""
+PS3="Select the type of commit: "
+OPTIONS=(feature: bug: test: documentation:)
+
+echo "${RED}############# STARTING COMMIT #############${ENDCOLOR}"
+
+while true; do
+    read -p "Stage all files?" yn
+    case $yn in
+        [Yy]* ) git add . && git diff --name-only --cached && break;;
+        [Nn]* ) git diff --name-only --cached && exit;;
+        * ) echo "Please answer y(es) or n(o).";;
+    esac
+done
+
+select commit_type in "${OPTIONS[@]}"
+do
+    COMMIT_TYPE=$commit_type
+    break
+done
+
+read -p "JIRA: " JIRA
+
+read -p "Message: " MESSAGE
+
+echo $COMMIT_TYPE;
+echo $JIRA
+
+git commit -m "${COMMIT_TYPE} ${MESSAGE}" -m ${JIRA}
+
+git log -1 HEAD --stat
+echo "${RED}############################################################${ENDCOLOR}"
+echo "Pushing changes to ${GREEN}origin/$(git rev-parse --abbrev-ref HEAD)${ENDCOLOR}"
+git push origin $(git rev-parse --abbrev-ref HEAD)
